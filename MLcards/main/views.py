6d02358b666efd2bcbc9c6b22bf5064776e706db
ninetzy <smolaninovvelizar@gmail.com>
2026-02-17 21:34:05 +0300

    Создание первых HTML шаблонов

--- MLcards/main/views.py
+++ MLcards/main/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h4>Main message</h4>")
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("<h4>Message about us</h4>")
+    return render(request, 'main/about.html')
